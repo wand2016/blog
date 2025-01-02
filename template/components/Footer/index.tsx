020d0817658908ec16f7d6c679e70b05bc1296b7
wand2016 <d19921207@gmail.com>
2025-01-02 23:07:44 +0900

    fix: replace placeholder imgs and stub strings

--- template/components/Footer/index.tsx
+++ template/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.cr}>© SIMPLE. All Rights Reserved 2023</p>
+      <p className={styles.cr}>&copy; 2025 wand</p>
     </footer>
   );
 }
