98716f188bf5323cd9113b43eb4e79ec45e05c13
wand2016 <d19921207@gmail.com>
2025-01-13 01:57:30 +0900

    fix: exclude favicon from sitemap.xml

--- template/next-sitemap.config.js
+++ template/next-sitemap.config.js
@@ -15,4 +15,18 @@ module.exports = {
       ],
     },
   ],
+  transform: async (config, loc) => {
+    // NOTE: app router で管理している favicon 類に trailing slash がついてしまう。
+    // そもそもページではないので除外する
+    if ([/apple-icon.png/, /manifest.webmanifest/, /icon.svg/].some((pattern) => pattern.test(loc)))
+      return null;
+
+    return {
+      loc, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
