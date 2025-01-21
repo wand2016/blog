7519e776433ddd2a718e0ae2da70fe6d625f51d2
wand2016 <d19921207@gmail.com>
2025-01-21 22:37:14 +0900

    fix: add anchor before iframe

--- template/libs/formatRichText.ts
+++ template/libs/formatRichText.ts
@@ -2,6 +2,7 @@ import 'highlight.js/styles/github.css';
 import cheerio from 'cheerio';
 import hljs from 'highlight.js';
 import { formatImageSrc } from '@/libs/formatImageSrc';
+import { parse } from 'qs';
 
 export const formatRichText = async (richText: string) => {
   const $ = cheerio.load(richText);
@@ -32,20 +33,30 @@ export const formatRichText = async (richText: string) => {
     }
   });
 
-  const iframeAnchorElements = $(
+  const iframelyAnchorElements = $(
     'div.iframely-embed > div.iframely-responsive > a[data-iframely-url]',
   ).get();
-  for (const elm of iframeAnchorElements) {
-    const iframelyUrl = $(elm).attr('data-iframely-url') ?? '';
+  for (const iframelyAnchorElement of iframelyAnchorElements) {
+    const iframelyUrl = $(iframelyAnchorElement).attr('data-iframely-url') ?? '';
     const iframelyUrlQueryParams = iframelyUrl.slice(iframelyUrl.indexOf('?'));
-
     const data = await fetch(
       `https://cdn.iframe.ly/api/iframely${iframelyUrlQueryParams}&omit_script=1&iframe=1&title=1`,
     );
     const json = await data.json();
     const html = json['html'];
 
-    $(elm).parent().parent().replaceWith(html);
+    const $replacement = cheerio.load(html);
+
+    // NOTE: iframely の iframe は静的にはリンクに見えないので、 a[hidden] を併記する
+    const params = parse(iframelyUrlQueryParams, { ignoreQueryPrefix: true });
+    const url = decodeURI(params.url as string);
+    const title = cheerio.load(html)('iframe').attr('title');
+    $replacement('iframe').before('<a hidden="hidden"></a>');
+    $replacement('a[hidden]')
+      .attr('href', url)
+      .text(title ?? '');
+
+    $(iframelyAnchorElement).parent().parent().replaceWith($replacement.html());
   }
 
   $('script[src="//cdn.iframe.ly/embed.js"]').remove();
