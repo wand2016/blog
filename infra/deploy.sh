5f358e6eca831fc8b8e0210c0e461e5e71b7c84c
wand2016 <d19921207@gmail.com>
2025-01-26 20:10:14 +0900

    fix: deploy.sh exits with 1 when invalid DOMAIN given

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -15,5 +15,10 @@ HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name=${DOMAIN} --qu
 
 echo "HOSTED_ZONE_ID is ${HOSTED_ZONE_ID}."
 
+if [ -z "${HOSTED_ZONE_ID:-}" ]; then
+  echo "no hosted zone for ${DOMAIN} exists."
+  exit 1
+fi
+
 sam build && sam deploy --stack-name=blog-${ENVIRONMENT} \
   --parameter-overrides Environment=${ENVIRONMENT} Domain=${DOMAIN} HostedZoneId=${HOSTED_ZONE_ID}
