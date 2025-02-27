f85e160d7d208495af0c3e5aa5f433585de5bb55
wand2016 <d19921207@gmail.com>
2025-02-27 13:00:59 +0900

    fix: force generate css

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  safelist: ['alert-info', 'alert-warn'],
   theme: {
     extend: {},
   },
