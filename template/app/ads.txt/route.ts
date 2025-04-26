2cb7e6b2a27985c79f16ba3bb22994cc9aac8a95
wand2016 <d19921207@gmail.com>
2025-04-26 22:39:44 +0900

    fix: specify static-export

--- template/app/ads.txt/route.ts
+++ template/app/ads.txt/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-static';
+
 export function GET() {
   return new NextResponse(
     `google.com, ${process.env.GOOGLE_ADSENSE_PUBLISHER_ID}, DIRECT, f08c47fec0942fa0`,
