78c04ffe7298c7af3466b552e2890df1c6612fe1
D.Horiyama <22491120+wand2016@users.noreply.github.com>
2025-04-28 22:21:46 +0900

    build: bump date-fns and date-fns-tz (#194)

--- template/libs/formatDate.ts
+++ template/libs/formatDate.ts
@@ -1,8 +1,8 @@
-import { utcToZonedTime } from 'date-fns-tz';
-import { format } from 'date-fns';
+import { format } from 'date-fns/format';
+import { toZonedTime } from 'date-fns-tz';
 
 export const formatDate = (date: string) => {
   const utcDate = new Date(date);
-  const jstDate = utcToZonedTime(utcDate, 'Asia/Tokyo');
+  const jstDate = toZonedTime(utcDate, 'Asia/Tokyo');
   return format(jstDate, 'yyyy-MM-dd');
 };
