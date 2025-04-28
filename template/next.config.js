e117b637c8f6d7c8610d208f5881a4a34cec21ae
wand2016 <d19921207@gmail.com>
2025-04-28 23:29:33 +0900

    uiux: enable experimental ViewTransition

--- template/next.config.js
+++ template/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  experimental: {
+    viewTransition: true,
+  },
 };
 
 module.exports = nextConfig;
