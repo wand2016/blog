0579fd4776c7265258792ab4e59feb9ff61db239
wand2016 <d19921207@gmail.com>
2025-04-28 21:14:09 +0900

    feat: 関連記事

--- template/app/articles/[slug]/page.tsx
+++ template/app/articles/[slug]/page.tsx
@@ -1,11 +1,12 @@
 import { Metadata, ResolvingMetadata } from 'next';
 
 import Article from '@/components/Article';
+import ArticleList from '@/components/ArticleList';
 import { extractHeadings } from '@/libs/extractHeadings';
 import { formatImageSrc } from '@/libs/formatImageSrc';
 import { formatRichText } from '@/libs/formatRichText';
 import { getGlobalTags } from '@/libs/getGlobalTags';
-import { getAllBlogIds, getDetail } from '@/libs/microcms';
+import { getAllBlogIds, getDetail, getList } from '@/libs/microcms';
 
 type Props = {
   params: Promise<{
@@ -45,23 +46,43 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: Props) {
   const { slug } = await params;
-  const data = await getDetail(slug);
+  const articleData = await getDetail(slug);
+  const tagId = articleData.tags?.[0]?.id;
+  const relatedArticles =
+    tagId == null
+      ? null
+      : (
+          await getList({
+            limit: 3,
+            filters: `tags[contains]${tagId}[and]id[not_equals]${articleData.id}`,
+          })
+        ).contents;
 
   // avoid `/` duplication
   const baseUrlRaw = process.env.BASE_URL ?? '';
   const baseUrl = baseUrlRaw.endsWith('/') ? baseUrlRaw : `${baseUrlRaw}/`;
 
-  const content = await formatRichText(data.content);
+  const content = await formatRichText(articleData.content);
   const headings = extractHeadings(content);
 
   return (
-    <Article
-      data={data}
-      formattedContent={content}
-      headings={!!data.use_toc ? headings : undefined}
-      shareUrl={`${baseUrl}articles/${slug}/`}
-      googleAdsensePublisherId={process.env.GOOGLE_ADSENSE_PUBLISHER_ID}
-      adSlotDisplayHorizontal={process.env.GOOGLE_ADSENSE_SLOT_DISPLAY_HORIZONTAL}
-    />
+    <>
+      <Article
+        data={articleData}
+        formattedContent={content}
+        headings={!!articleData.use_toc ? headings : undefined}
+        shareUrl={`${baseUrl}articles/${slug}/`}
+        googleAdsensePublisherId={process.env.GOOGLE_ADSENSE_PUBLISHER_ID}
+        adSlotDisplayHorizontal={process.env.GOOGLE_ADSENSE_SLOT_DISPLAY_HORIZONTAL}
+      />
+      {relatedArticles && (
+        <aside>
+          <section aria-labelledby="related-posts-heading">
+            <h2 className="text-xl font-bold mb-2">関連記事</h2>
+            <ArticleList articles={relatedArticles} />
+          </section>
+        </aside>
+      )}
+    </>
   );
 }
