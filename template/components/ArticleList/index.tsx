81b84e5cb75a34ffabf42ddcb3a14db520b03537
wand2016 <d19921207@gmail.com>
2025-06-13 09:36:22 +0900

    fix: lazy load only for related articles

--- template/components/ArticleList/index.tsx
+++ template/components/ArticleList/index.tsx
@@ -8,9 +8,19 @@ type Props = {
   articles: readonly Omit<Article, 'related_articles'>[];
   /** @default true */
   enableViewTransition?: boolean;
+  /**
+   * 記事サムネイル画像の読み込み・描画を遅延させます。
+   * 記事下部の「関連記事」など、オフスクリーンの画像読み込みを遅延させるために使用します。
+   * @default false
+   */
+  lazy?: boolean;
 };
 
-export default function ArticleList({ articles, enableViewTransition = true }: Props) {
+export default function ArticleList({
+  articles,
+  enableViewTransition = true,
+  lazy = false,
+}: Props) {
   if (articles.length === 0) {
     return <p>記事がありません。</p>;
   }
@@ -18,7 +28,11 @@ export default function ArticleList({ articles, enableViewTransition = true }: P
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       {articles.map((article, index) => (
         <Fragment key={article.id}>
-          <ArticleListItem article={article} enableViewTransition={enableViewTransition} lazy />
+          <ArticleListItem
+            article={article}
+            enableViewTransition={enableViewTransition}
+            lazy={lazy}
+          />
           {!!process.env.GOOGLE_ADSENSE_PUBLISHER_ID &&
             !!process.env.GOOGLE_ADSENSE_SLOT_IN_FEED &&
             (index === 3 || index === 8) && (
