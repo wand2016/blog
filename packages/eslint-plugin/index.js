692d4d088cc01332d83eb87f3dd694ef165a68b7
wand2016 <d19921207@gmail.com>
2026-01-29 23:26:42 +0900

    fix: config

--- packages/eslint-plugin/index.js
+++ packages/eslint-plugin/index.js
@@ -7,16 +7,16 @@ module.exports = {
         plugins: {
           wandfuldays: {
             rules: {
-              "wandfuldays/app-router-params-prop-promise": "error",
-              "wandfuldays/no-redundant-template-literal": "error",
+              "app-router-params-prop-promise": require("./rules/app-router-params-prop-promise"),
+              "no-redundant-template-literal": require("./rules/no-redundant-template-literal"),
             },
           },
         },
+        rules: {
+          "wandfuldays/app-router-params-prop-promise": "error",
+          "wandfuldays/no-redundant-template-literal": "error",
+        },
       },
     ],
-  },
-  rules: {
-    "app-router-params-prop-promise": require("./rules/app-router-params-prop-promise"),
-    "no-redundant-template-literal": require("./rules/no-redundant-template-literal"),
   },
 };
